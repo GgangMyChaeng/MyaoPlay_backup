1910766d15c2aeae272aa1ca803f7191a8d8137c
GgangMyChaeng <jpengempole112@gmail.com>
2026-01-20 18:05:17 +0900

    Update ui_modal.js

--- modules/ui_modal.js
+++ modules/ui_modal.js
@@ -154,8 +154,7 @@ export async function openModal() {
   setO("display", "block");
   setO("overflow", "auto");
   setO("-webkit-overflow-scrolling", "touch");
-  setO("background", "rgba(0,0,0,.55)");
-  setO("z-index", "90");
+  setO("z-index", "90"); // modal z-index
   setO("padding", "0"); // 4) modal이 margin/pad 갖고 있으니 overlay는 0
   host.appendChild(overlay);
   // 5) 컨테이너 기준으로 사이징
