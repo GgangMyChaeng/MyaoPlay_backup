1383d21c1ea1d4be35111574d7b76d16e1a4263f
GgangMyChaeng <jpengempole112@gmail.com>
2026-01-27 18:15:04 +0900

    Update utils.js

--- modules/utils.js
+++ modules/utils.js
@@ -118,6 +118,8 @@ export function makeAsstSig(text) {
 // TTS로 보내기 전에 불필요한 요소 제거
 export function preprocessForTts(text) {
   let t = String(text || "");
+  // 0) HTML/XML 태그 전부 제거 (state, StatusPlaceHolderImpl 등)
+  t = t.replace(/<[^>]*>/g, " ");
   // 1) *지문/액션* 제거 (별표로 감싼 부분)
   t = t.replace(/\*[^*]+\*/g, " ");
   // 2) 마크다운 제거
